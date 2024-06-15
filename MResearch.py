dce32a39f828bdf06f7e7f7b58c3b41737e8b66a
Moses M <116585974+MosesMwalya@users.noreply.github.com>
2024-06-15 23:25:17 +0300

    Update MResearch.py

--- MResearch.py
+++ MResearch.py
@@ -14,22 +14,22 @@ st.write("""
         
          """)
 
-msgs = ['If you could have any superpower, what would it be and why? ',
-'What is your favorite book or movie, and what makes it special to you? ',
-'If you could travel anywhere in the world, where would you go and what would you do there? ',
-'What's the most unusual or interesting job you've ever had? ',
-'If you could meet any historical figure, who would it be and what one question would you ask them? ',
-'What is one thing on your bucket list that you hope to accomplish in the next five years? ',
-'If you were an animal, which one would you be and why? ',
-'What’s a hobby or activity you’ve always wanted to try but haven’t yet? ',
-'If you could instantly become an expert in any field, what would it be and why? ',
-'What's the best piece of advice you've ever received, and how has it impacted you? ',
-'If you were a season, which one would you be and why? ',
-'If you could magically learn any language, which one would you choose and why? ',
-'What’s your favorite way to spend a day off? ',
-'If you could swap lives with anyone for a day, who would it be and what would you do? ',
-'What's the weirdest food you've ever tried, and did you like it? ',
-'What’s one talent you wish you had?'
+msgs = ["If you could have any superpower, what would it be and why? ",
+"What is your favorite book or movie, and what makes it special to you? ",
+"If you could travel anywhere in the world, where would you go and what would you do there? ",
+"What is the most unusual or interesting job you've ever had? ",
+"If you could meet any historical figure, who would it be and what one question would you ask them? ",
+"What is one thing on your bucket list that you hope to accomplish in the next five years? ",
+"If you were an animal, which one would you be and why? ",
+"What is a hobby or activity you’ve always wanted to try but haven’t yet? ",
+"If you could instantly become an expert in any field, what would it be and why? ",
+"What is the best piece of advice you've ever received, and how has it impacted you? ",
+"If you were a season, which one would you be and why? ",
+"If you could magically learn any language, which one would you choose and why? ",
+"What is your favorite way to spend a day off? ",
+"If you could swap lives with anyone for a day, who would it be and what would you do? ",
+"What is the weirdest food you've ever tried, and did you like it? ",
+"What is one talent you wish you had? "
         ]
 
 
