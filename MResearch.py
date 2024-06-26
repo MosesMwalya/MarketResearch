2ffcb68da621bf1cabb322943ae3781bea6edef1
Moses M <116585974+MosesMwalya@users.noreply.github.com>
2024-06-26 09:42:10 +0300

    Update MResearch.py

--- MResearch.py
+++ MResearch.py
@@ -14,23 +14,15 @@ st.write("""
         
          """)
 
-msgs = ["If you could have any superpower, what would it be and why? ",
-"What is your favorite book or movie, and what makes it special to you? ",
-"If you could travel anywhere in the world, where would you go and what would you do there? ",
-"What is the most unusual or interesting job you've ever had? ",
-"If you could meet any historical figure, who would it be and what one question would you ask them? ",
-"What is one thing on your bucket list that you hope to accomplish in the next five years? ",
-"If you were an animal, which one would you be and why? ",
-"What is a hobby or activity you’ve always wanted to try but haven’t yet? ",
-"If you could instantly become an expert in any field, what would it be and why? ",
-"What is the best piece of advice you've ever received, and how has it impacted you? ",
-"If you were a season, which one would you be and why? ",
-"If you could magically learn any language, which one would you choose and why? ",
-"What is your favorite way to spend a day off? ",
-"If you could swap lives with anyone for a day, who would it be and what would you do? ",
-"What is the weirdest food you've ever tried, and did you like it? ",
-"What is one talent you wish you had? "
-        ]
+msgs = [
+ "as a Genz, What worries do you have?",
+ "as a Genz, what are your expectations from your parents and other relatives",
+ "as a Genz, how does your day look like, starting from when you wake up to the time you sleep",
+ "as a Genz, What are some things your parents and older relatives have told you that don't make sense?"
+ "as a Genz, What is your preferred way to rewind?",
+ "as a Genz, if you could meet any historical figure, who would it be and what one question would you ask them?",
+ "as a Genz, what brands would you love to be associated with?",
+ "as a Genz, What is one talent you wish you had?"]
 
 
 f_name = st.text_input('Enter your first name')
@@ -42,4 +34,4 @@ if st.button('SUBMIT'):
     name = f_name.capitalize()
     msg = msgs[intpos]
     
-    st.write(name +" Welcome at Research team meeting! Your presence is valued. "+msg)
+    st.write(name +" Karibu sana to Research team meeting! Your presence is valued. "+msg)
