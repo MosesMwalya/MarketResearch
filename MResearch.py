0e76701ebed49ceef6b7d93974dea9d9d4c14bfb
Moses M <116585974+MosesMwalya@users.noreply.github.com>
2024-06-26 09:44:51 +0300

    Update MResearch.py

--- MResearch.py
+++ MResearch.py
@@ -15,14 +15,14 @@ st.write("""
          """)
 
 msgs = [
- "as a Genz, What worries do you have?",
- "as a Genz, what are your expectations from your parents and other relatives",
- "as a Genz, how does your day look like, starting from when you wake up to the time you sleep",
- "as a Genz, What are some things your parents and older relatives have told you that don't make sense?"
- "as a Genz, What is your preferred way to rewind?",
- "as a Genz, if you could meet any historical figure, who would it be and what one question would you ask them?",
- "as a Genz, what brands would you love to be associated with?",
- "as a Genz, What is one talent you wish you had?"]
+ " As a Genz, What worries do you have?",
+ " As a Genz, what are your expectations from your parents and other relatives",
+ " As a Genz, how does your day look like, starting from when you wake up to the time you sleep",
+ " As a Genz, What are some things your parents and older relatives have told you that don't make sense?"
+ " As a Genz, What is your preferred way to rewind?",
+ " As a Genz, if you could meet any historical figure, who would it be and what one question would you ask them?",
+ " As a Genz, what brands would you love to be associated with?",
+ " As a Genz, What is one talent you wish you had?"]
 
 
 f_name = st.text_input('Enter your first name')
